dc34aee6d7da1c3b08b508d7c317ad868ab015aa
Samuel-Kwon <samuel.seonje.kwon@gmail.com>
2020-06-15 23:44:34 -0400

    Third Project added and Open Github button added on pop-up screen

--- javascript/main.js
+++ javascript/main.js
@@ -10,13 +10,21 @@ function DisplayPopUpScreen2()
     document.getElementById("popUpScreen2").style.display = 'block';
 }
 
+function DisplayPopUpScreen3()
+{
+    document.getElementById("overlay").style.display = 'block';
+    document.getElementById("popUpScreen3").style.display = 'block';
+}
+
 function ClosePopUpScreen()
 {
-    document.getElementById("overlay").style.display = 'none';
     document.getElementById("popUpScreen1").style.display = 'none';
+    document.getElementById("popUpScreen2").style.display = 'none';
+    document.getElementById("popUpScreen3").style.display = 'none';
+    
     document.getElementById("video-avoidRock").pause();
     document.getElementById("video-avoidRock").currentTime = 0;
 
     document.getElementById("overlay").style.display = 'none';
-    document.getElementById("popUpScreen2").style.display = 'none';
+
 }
